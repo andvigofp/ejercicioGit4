f53dc2a0c73f2a0315787bebae1b0f668b1eae85
andvigo <animacion96@gmail.com>
2022-12-01 21:25:30 +0100

    ahora hay dos campos inmutables

--- src/Empleado.java
+++ src/Empleado.java
@@ -3,9 +3,9 @@ import java.util.Date;
  * @Author: Andrés Fernández Pereira
  */
 class Empleado {
-    String nombre;
+   final String nombre;
     int edad;
-    Date fechaContratacion;
+   final Date fechaContratacion;
 
     public Empleado(String nombre, int edad, Date fechaContratacion) {
         this.nombre = nombre;
