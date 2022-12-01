e15a1b56a79e503af8480f89af69697f2270cae8
andvigo <animacion96@gmail.com>
2022-12-01 21:40:45 +0100

    information hidding: edad es solo modificable a través de un setter

--- src/Empleado.java
+++ src/Empleado.java
@@ -4,7 +4,7 @@ import java.util.Date;
  */
 class Empleado {
     String nombre;
-    int edad;
+    private int edad;
     Date fechaContratacion;
 
     public Empleado(String nombre, int edad, Date fechaContratacion) {
@@ -12,7 +12,10 @@ class Empleado {
         this.edad = edad;
         this.fechaContratacion = fechaContratacion;
     }
-
+    public void setEdad(int edad) {
+        if (edad<0) throw new RuntimeException("Un empleado no puede tener edad negativa");
+        else this.edad = edad;
+    }
     @Override
     public String toString() {
         return "Se ha registrado a " + nombre + " (" + edad + " años; contratado en 19" + fechaContratacion.getYear() + ")";
